7da41368405245c80d9e0ddccfcfab9a31dc38d2
Jonathan Corado <a.corado3p@gmail.com>
2020-08-31 20:43:24 -0600

    push ali

---
+++ status.php
@@ -0,0 +1,49 @@
+<?php
+
+    $orden = $_GET["orden"];
+    $tienda = $_GET["tienda"];
+    $formato = $_GET["formato"];
+    
+    $dbconn = pg_connect("host=localhost dbname=Proyecto1CC6 user=postgres password=12345")
+    or die('No se ha podido conectar: ' . pg_last_error());
+
+    if(strtoupper($formato) == "XML"){
+         $xml = new SimpleXMLElement('<orden/>');
+         $courrier = $xml->addChild('courrier', "mcqueen");
+         $ord = $xml->addChild('orden', $orden);
+        $query = "SELECT estado from Envio WHERE codigo=$orden AND tienda='$tienda'";
+        $result = pg_query($query);
+        if(pg_num_rows($result) > 0){
+        while($line = pg_fetch_assoc($result)){
+            $estado = $line["estado"];
+         $est = $xml->addchild('estatus', $estado);
+        }
+       
+        Header('Content-type: text/xml');
+        print($xml->asXML());
+    }else{
+
+
+
+    }
+
+    }else if(strtoupper($formato) == "JSON"){
+        $est=0;
+        $query = "SELECT estado from Envio WHERE codigo=$orden AND tienda='$tienda'";
+        $result = pg_query($query);
+        if(pg_num_rows($result) > 0){
+        while($line = pg_fetch_assoc($result)){
+            $est = $line["estado"];
+        }
+        $lleva = new stdClass();
+        $lleva->orden = array("courier"=>"mcqueen", "orden"=> intval($orden)  ,"estatus" => intval($est) );
+
+        $json = json_encode($lleva);
+        
+        print($json);
+    }else{
+        
+    }
+    }
+
+?>
