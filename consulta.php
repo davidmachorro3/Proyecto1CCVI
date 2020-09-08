0bd451623514ceb88352fafeb650313bc816686c
EduardoCalderon9 <46823752+EduardoCalderon9@users.noreply.github.com>
2020-09-07 22:10:17 -0600

    Add files via upload

--- consulta.php
+++ consulta.php
@@ -11,15 +11,16 @@
     or die('No se ha podido conectar: ' . pg_last_error());
     $query = "SELECT costo FROM destino WHERE codigo = '$destino'";
     $result = pg_query($query);
+	$mod = rand(1,300);
 	
     if(strtoupper($formato) == "XML"){
         $xml = new SimpleXMLElement('<consultaprecio/>');
         $courrier = $xml->addChild('courrier', "mcqueen");
         $dest = $xml->addChild('destino', $destino);
         if(pg_num_rows($result) > 0){
-            $costo = $costo = pg_fetch_assoc($result)[strval('costo')];
+            $costo = pg_fetch_assoc($result)[strval('costo')];
             $cobertura = $xml->addchild('cobertura', "TRUE");
-            $cost = $xml->addchild('costo', $costo);
+            $cost = $xml->addchild('costo', $costo + $mod);
         }else{
             $cobertura = $xml->addchild('cobertura', "FALSE");
             $cost = $xml->addchild('costo', "0");
@@ -28,8 +29,8 @@
     }else if(strtoupper($formato) == "JSON"){
         $preciojson = new stdClass();
         if(pg_num_rows($result) > 0){
-            $costo = $costo = pg_fetch_assoc($result)['costo'];
-            $preciojson->consultaprecio = array("courrier"=>"mcqueen", "destino"=>$destino  ,"cobertura" => "TRUE", "costo"=> strval($costo));
+            $costo = pg_fetch_assoc($result)['costo'];
+            $preciojson->consultaprecio = array("courrier"=>"mcqueen", "destino"=>$destino  ,"cobertura" => "TRUE", "costo"=> strval($costo + $mod));
         }else{
             $preciojson->consultaprecio = array("courrier"=>"mcqueen", "destino"=>$destino  ,"cobertura" => "FALSE", "costo"=> "0");
         }
